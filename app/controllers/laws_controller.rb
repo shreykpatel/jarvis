186d3c3dd028c6fbf3530995b3a6e89c9d315ee8
Akshat <akshat.shah@go-jek.com>
2017-04-22 19:21:25 +0530

    persistence

--- app/controllers/laws_controller.rb
+++ app/controllers/laws_controller.rb
@@ -1,16 +1,32 @@
 class LawsController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	def create
-    laws_of_all_cities = Array.new
+    laws = Array.new
 		(1..6).each do |city|
-			laws_of_all_cities << get_laws_of_a_city("http://nyaaya.in/api/e1/traffic-challan/?city=1")
+      laws_of_a_city = get_laws_of_a_city("http://nyaaya.in/api/e1/traffic-challan/?city=#{city}")
+      persist_laws_of_a_city("traffic-challan", city, laws_of_a_city)
+			laws << laws_of_a_city
     end
 
-    puts laws_of_all_cities
-		return render(result: laws_of_all_cities, status: :ok)
+    return render(nothing: true, status: :ok)
 	end
 
-	private
+  def persist_laws_of_a_city(genre, location, laws)
+    laws.each do |law|
+      Law.create(genre: "traffic-challan",
+                 location: location,
+                 offense: law["traffic_offense"],
+                 description: law["simplified"],
+                 fine_first_offense: law["fine_first_offense"],
+                 prison_first_offense: law["jail_first_offense"],
+                 fine_second_offense: law["fine_second_offense"],
+                 prison_second_offense: law["jail_second_offense"],
+                 additional_information: law["hyperlink"],
+                 intents: law["category"]["name"])
+    end
+  end
+
+  private
 
 	def get_laws_of_a_city(request_for_laws)
     final_response = Array.new
